3b52e68a551e34184fa7c4aec62abdf1aa20ccff
Jialin Ma <marlin-@gmx.cn>
2018-12-22 06:05:22 -0500

    Fix geturls.sh

--- geturls.sh
+++ geturls.sh
@@ -2,8 +2,8 @@
 
 downurls () {
     while read sitemaplink; do
-        curl -s $sitemaplink | grep https | sed 's/^.*<loc>//' | sed 's/<\/loc>.*$//'
+        curl -s $sitemaplink | sed 's/<loc>/\n<loc>/g' | grep https | sed 's/^.*<loc>//' | sed 's/<\/loc>.*$//'
     done
 }
 
-curl -s https://h2g2.club/sitemap.xml | grep https | sed 's/^ *<loc>//' | sed 's/<\/loc>//' | downurls
+curl -s https://h2g2.club/sitemap.xml | sed 's/<loc>/\n<loc>/g' | grep https | sed 's/^ *<loc>//' | sed 's/<\/loc>//' | downurls
