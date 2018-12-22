e04bd62a8779fee944a51a2975a9b9560ca4711d
Jialin Ma <marlin-@gmx.cn>
2018-12-22 11:33:02 +0000

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
- curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=h2g2.club&token=8384ZYxY1PNc8NyE"
+
+Nurl=`wc -l urls.txt | awk '{print $1}'`
+Date=`date`
+
+echo
+echo
+echo ====== Submit "$Nurl" urls === "$Date" ======
+
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=h2g2.club&token=8384ZYxY1PNc8NyE"
+
+echo
+echo ====== Done ======
+echo
